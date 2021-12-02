41786b2ebdcab5fd70c79939e668a93b87c419d0
voltarcarnage <whiteisdead@mail.ru>
2021-12-02 22:14:57 +0300

    =)

--- Source/enemy.cpp
+++ Source/enemy.cpp
@@ -2,20 +2,43 @@
 
 namespace Game_N{
 
-  Enemy::Enemy()
+  Enemy::Enemy(sf::Vector2i pos)
   {
+    alive_ = true;
+  	detectPlayer_ = false;
+  	position_ = pos;
+  	maxHp_ = 100;
+    name_ = "Enemy";
+    hp_ = maxHp_;
+  	lvl_ = 0;
+  	strength_ = 5;
+  	range_ = 3;
+  	movingUp_ = false;
+  	movingDown_ = false;
+  	movingLeft_ = false;
+  	movingRight_ = false;
+  }
+
+  Enemy::Enemy(int lvl, int strength, int range, int damage, int x, int y, std::string name, sf::Vector2i pos)
+  {
+    alive_ = true;
+    lvl_ = lvl;
+    strength_ = strength;
+    range_ = range;
+    damage_ = damage;
+    xCoord_ = x;
+    yCoord_ = y;
+    name_ = name;
+    position_ = pos;
+
+    movingUp_ = false;
+  	movingDown_ = false;
+  	movingLeft_ = false;
+  	movingRight_ = false;
 
   }
 
-  void Enemy::gainLvl(int lvl)
-  {
-    lvl_ += lvl;
-    maxHp_ = maxHp_ * 1.3 * lvl_ * 0.5;
-  	hp_ = hp_ * 1.3 * lvl * 0.5;
-  	damage_ += 2;
-  	// agility+=2;
-  	// defense+=2;
-  }
+  // void Enemy::
 
   bool Enemy::takeDamage(int dmg)
   {
--- Source/headers/enemy.h
+++ Source/headers/enemy.h
@@ -1,3 +1,5 @@
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics.hpp>
 
 #include "entity.h"
 #include "spells.h"
@@ -14,26 +16,34 @@ namespace Game_N{
       int strength_;
       int range_;
       int damage_;
+      sf::Vector2i position_;
+		  sf::Vector2i targetPos_;
+      bool movingUp_,movingDown_,movingLeft_,movingRight_,faceUp_, faceDown_,faceLeft_,faceRight_;
       Necromancy necromancy;
     public:
-      Enemy(); //SFML
-      // Enemy(int lvl, int strength, int range, int damage, int x, int y); SFML
+      Enemy(sf::Vector2i pos); //SFML
+      Enemy(int lvl, int strength, int range, int damage, int x, int y, std::string name, sf::Vector2i pos);
 
       int getLvl() const {return lvl_;}
       int getStrength() const {return strength_;}
       int getRange() const {return range_;}
       int getDamage() const {return damage_;}
+      sf::Vector2i getPos() const {return position_;}
 
       void setLvl(int lvl) {lvl_ = lvl;}
       void setStrength(int strength) {strength_ = strength;}
       void setRange(int range) {range_ = range;}
       void setDamage(int damage) {damage_ = damage;}
+      void setPos(unsigned int x, unsigned int y);
+      void setImg(sf::Sprite& img);
+
+      void setTargetPos(sf::Vector2i pos);
+		  bool isFreshTarget();
+		  void detectHero(sf::Vector2i pos);
+		  void undetectHero();
 
       void gainLvl(int lvl);
 
-      // void followToHero();
-      void getPos(); //SFML vector
-      // void setPos(int x, int y) {xCoord_ = x; yCoord_ = y;}
       bool takeDamage(int dmg);
       bool isAlive();
       void die();
@@ -51,8 +61,8 @@ namespace Game_N{
 
       bool canMove(std::vector<std::vector<int>>& map);
       void moveToTarget(std::vector<std::vector<int>>& map);
-		  // int faceWhere();
-		  // void faceThere(int dir);
+		  int faceWhere();
+		  void faceThere(int dir);
 
   };
 
